5145259bbcc2b1bee463045ef33be4526970719c
Vishalbgmi <vishalbgmi699@gmail.com>
2026-09-07 08:25:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,12 +31,12 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8916117566:AAH2p58FbI1XAzKH-hdKOOmLaV9jmMsXuz0")
 MONGODB_URI = os.getenv("MONGODB_URI")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "attack_bot")
 API_URL = os.getenv("API_URL")
-API_KEY = os.getenv("API_KEY")
-ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "1793697840").split(",")]
+API_KEY = os.getenv("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")
+ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "5268737363").split(",")]
 
 # Blocked ports (must match backend)
 BLOCKED_PORTS = {8700, 20000, 443, 17500, 9031, 20002, 20001}
